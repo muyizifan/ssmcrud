d4812701c42a552ca8cb4f5572971417ff6302cb
muyizifan <839819497@qq.com>
2017-09-14 19:02:33 +0800

    后台向前台返回json的分页数据

--- src/main/java/org/muizifan/crud/controller/EmployeeController.java
+++ src/main/java/org/muizifan/crud/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package org.muizifan.crud.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.muizifan.crud.bean.Employee;
+import org.muizifan.crud.bean.Msg;
 import org.muizifan.crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,11 +23,27 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @RequestMapping("/emps")
+    @ResponseBody  //自动将数据转为json格式
+    public Msg getEmpsWithJson(@RequestParam(value = "pn",defaultValue = "1") Integer pn){
+        //这不是一个分页查询
+        //引入PageHelper分页插件
+        //在查询之前只需要调用,传入页码，以及每页的大小
+        PageHelper.startPage(pn,5);
+        //startPage后面紧跟的这个查询就是一个分页查询
+        List<Employee> emps = employeeService.getAll();
+        //使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
+        //封装了详细的分页信息，包括有我们查询出来的数据,传入连续现实的页数
+        PageInfo page = new PageInfo(emps,5);
+        return Msg.success().add("pageInfo",page);
+    }
+
     /**
      * 查询员工数据(分页查询)
      * @return
      */
-    @RequestMapping("/emps")
+    //@RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn",defaultValue = "1") Integer pn,Model model){
         //这不是一个分页查询
         //引入PageHelper分页插件
